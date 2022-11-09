7bbc8d0580cd3d6c4d5e90db171fb33609195ded
Cesar Lucumi <clucumi.94@gmail.com>
2022-11-09 16:02:11 -0500

    Mobile Menu Merged

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
-    console.log('click');
+    // console.log('click');
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('Js Working');
+// console.log('Js Working');
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
