65df2ce4507a3d332e00092a2e3b91a04ec4cf5e
Cesar Lucumi <clucumi.94@gmail.com>
2022-11-11 13:56:56 -0500

    Shopping Cart  Merged

--- main.js
+++ main.js
@@ -2,18 +2,43 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click',toogleCartAside);
 
 function toggleDesktopMenu(){
     // console.log('click');
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 // console.log('Js Working');
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toogleCartAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+
 }
